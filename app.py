1d1b40c47a19ea924a42f44d4e356f76387dbcbe
UnknwonD <daeho5000@naver.com>
2024-06-18 21:52:28 +0900

    Youtube URL Download Fin

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def downloadYouTube(videourl, path):
     yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
     if not os.path.exists(path):
         os.makedirs(path)
-    yt.download(path)
+    yt.download(path, filename='youtube_video.mp4')
 
 
 app = Flask(__name__)
